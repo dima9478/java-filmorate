91cab72318e671c08e455ef864635dcc02a8173b
dima947 <tim-5695@mail.ru>
2023-06-24 13:26:26 +0300

    fix: change inserts to merge

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,23 +1,23 @@
-INSERT INTO ratings (name)
-VALUES ('G');
-INSERT INTO ratings (name)
-VALUES ('PG');
-INSERT INTO ratings (name)
-VALUES ('PG-13');
-INSERT INTO ratings (name)
-VALUES ('R');
-INSERT INTO ratings (name)
-VALUES ('NC-17');
+MERGE INTO ratings (rating_id, name)
+    VALUES (1, 'G');
+MERGE INTO ratings (rating_id, name)
+    VALUES (2, 'PG');
+MERGE INTO ratings (rating_id, name)
+    VALUES (3, 'PG-13');
+MERGE INTO ratings (rating_id, name)
+    VALUES (4, 'R');
+MERGE INTO ratings (rating_id, name)
+    VALUES (5, 'NC-17');
 
-INSERT INTO genres (name)
-VALUES ('Комедия');
-INSERT INTO genres (name)
-VALUES ('Драма');
-INSERT INTO genres (name)
-VALUES ('Мультфильм');
-INSERT INTO genres (name)
-VALUES ('Триллер');
-INSERT INTO genres (name)
-VALUES ('Документальный');
-INSERT INTO genres (name)
-VALUES ('Боевик');
+MERGE INTO genres (genre_id, name)
+    VALUES (1, 'Комедия');
+MERGE INTO genres (genre_id, name)
+    VALUES (2, 'Драма');
+MERGE INTO genres (genre_id, name)
+    VALUES (3, 'Мультфильм');
+MERGE INTO genres (genre_id, name)
+    VALUES (4, 'Триллер');
+MERGE INTO genres (genre_id, name)
+    VALUES (5, 'Документальный');
+MERGE INTO genres (genre_id, name)
+    VALUES (6, 'Боевик');
